655f563dc386d08d4a0a6a9e43edfc01b348d0e2
Rudolf Ádám <sari.vogel.94@gmail.com>
2019-01-20 21:46:46 +0100

    Add results.php

---
+++ results.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Naturreservat</title>
+</head>
+<body>
+
+<?php
+
+abstract class Animal {
+    protected $name;
+
+    function __construct($name) {
+        $this->name = $name;
+    }
+
+    abstract function makeSound();
+}
+
+class Monkey extends Animal {
+    public function makeSound() {
+        return $this->name." says mak-mak";
+    }
+}
+
+class Tiger extends Animal {
+    public function makeSound() {
+        return $this->name." says wááááááá";
+    }
+}
+
+class Giraffe extends Animal {
+    public function makeSound() {
+        return $this->name." says i am a giraffe";
+    }
+}
+
+class Coco {
+
+}
+
+?>
+
+<?php
+
+for ($i = 0; $i < $_GET["numberOfMonkeys"]; $i++) {
+    $monkey = new Monkey($i."monkey");
+    echo "<img src='img/monkey.png' onclick='alert(\"".$monkey->makeSound()."\")'/>\n";
+}
+
+
+for ($i = 0; $i < $_GET["numberOfGiraffes"]; $i++) {
+    $giraffe = new Giraffe($i."giraffe");
+    echo "<img src='img/giraffe.png' onclick='alert(\"".$giraffe->makeSound()."\")'/>\n";
+}
+
+
+for ($i = 0; $i < $_GET["numberOfTigers"]; $i++) {
+    $tiger = new Tiger($i."tiger");
+    echo "<img src='img/tiger.png' onclick='alert(\"".$tiger->makeSound()."\")'/>\n";
+}
+
+
+for ($i = 0; $i < $_GET["numberOfCocos"]; $i++) {
+    echo "<img src='img/coco.png'/>\n";
+}
+
+?>
+
+</body>
+</html>
+
--- settings.php
+++ settings.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Naturreservat</title>
+    <title>Naturreservat2</title>
 </head>
 <body>
     
+    <form action="results.php" method="get">
+    Apor: <input type="number" name="numberOfMonkeys"><br>
+    Giraffer: <input type="number" name="numberOfGiraffes"><br>
+    Tigrar: <input type="number" name="numberOfTigers"><br>
+    Kokosnötter: <input type="number" name="numberOfCocos"><br>
+    <input type="submit">
+    </form>
+
 </body>
 </html>
