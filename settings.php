828ce75557b79e6045106c50d929853d1786d004
Rudolf Ádám <sari.vogel.94@gmail.com>
2019-01-21 00:19:44 +0100

    Add style

--- settings.php
+++ settings.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Naturreservat2</title>
 </head>
 <body>
